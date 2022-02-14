62e9d833162124f890790dddd3a0b806f07a12c1
Yiran001 <korchflorian@gmail.com>
2022-02-14 16:37:19 +0100

    initial

---
+++ Garden-Service/src/Garden-Service.Core/Models/Garden.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+namespace Inplanticular.Garden_Service.Core.Models; 
+
+public class Garden {
+	public string Name { get; set; }
+	public DateTime DateOfCreation { get; set; }
+	public List<Plant> Plants { get; set; }
+	
+}
---
+++ Garden-Service/src/Garden-Service.Core/Models/Plant.cs
@@ -0,0 +1,32 @@
+﻿namespace Inplanticular.Garden_Service.Core.Models; 
+
+public class Plant {
+	public Plant(string friendlyName, string botanicalName, int daysToMature, double growthPerDay, double avgFruitWeight, int timeFromPlanting, int ripePercentage, double actFruitCount) {
+		FriendlyName = friendlyName;
+		BotanicalName = botanicalName;
+		DaysToMature = daysToMature;
+		GrowthPerDay = growthPerDay;
+		AvgFruitWeight = avgFruitWeight;
+		TimeFromPlanting = timeFromPlanting;
+		RipePercentage = ripePercentage;
+		ActFruitCount = actFruitCount;
+	}
+
+	//semi-final member. Not changed often
+	//general
+	public string FriendlyName { get; set; }
+	public string BotanicalName { get; set; }
+	//growth related
+	public int DaysToMature { get; set; }
+	public double GrowthPerDay { get; set; }
+	//yield related
+	public double AvgFruitWeight { get; set; }
+	
+	//often changed members
+	//growth related
+	public int TimeFromPlanting { get; set; }
+	public int RipePercentage { get; set; }
+	//yield related
+	public double ActFruitCount { get; set; }
+	
+}
