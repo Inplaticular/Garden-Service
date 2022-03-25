00ed1d5c359e9f511fa145d15b1de0798147b10a
Yiran001 <korchflorian@gmail.com>
2022-03-25 22:24:57 +0100

    added longitude and latitude properties to Garden

--- Garden-Service/src/Garden-Service.Core/Models/Garden.cs
+++ Garden-Service/src/Garden-Service.Core/Models/Garden.cs
@@ -4,18 +4,22 @@ public class Garden {
 	public Garden() {
 	}
 
-	public Garden(string name, string userId, DateTime dateOfCreation) {
+	public Garden(string name, string userId, double coordinateLatitude, double coordinateLongitude,
+		DateTime dateOfCreation) {
 		Id = Guid.NewGuid().ToString();
 		Name = name;
 		UserId = userId;
+		CoordinateLatitude = coordinateLatitude;
+		CoordinateLongitude = coordinateLongitude;
 		DateOfCreation = dateOfCreation;
 	}
 
 	public string Id { get; set; }
 	public string UserId { get; set; }
-
 	public string UnitId { get; set; }
 	public string Name { get; set; }
+	public double CoordinateLatitude { get; set; }
+	public double CoordinateLongitude { get; set; }
 	public DateTime DateOfCreation { get; set; }
 	public ICollection<Plant> Plants { get; }
 }
