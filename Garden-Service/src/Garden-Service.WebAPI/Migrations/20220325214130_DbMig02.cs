9c0760302b60fb9b603e4a32d9dd1fe3c0597ea4
Yiran001 <korchflorian@gmail.com>
2022-03-25 22:42:50 +0100

    added Migrations

---
+++ Garden-Service/src/Garden-Service.WebAPI/Migrations/20220325214130_DbMig02.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inplanticular.Garden_Service.WebAPI.Migrations
+{
+    public partial class DbMig02 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "CoordinateLatitude",
+                table: "Gardens",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "CoordinateLongitude",
+                table: "Gardens",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoordinateLatitude",
+                table: "Gardens");
+
+            migrationBuilder.DropColumn(
+                name: "CoordinateLongitude",
+                table: "Gardens");
+        }
+    }
+}
